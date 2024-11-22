211830e4285a646ad24ba70232ea3c71a9c970f1
Dorian <dorian.boussakine@gmail.com>
2024-11-23 00:23:45 +0200

    Asennettu supertest HTTP-pyynnön testausta varten

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     testEnvironment: 'node', // Koska tämä on Node.js-projekti
 
-    testMatch: ['**/*.index.test.js'], // Säädä tämä kuvio vastaamaan testitiedostosi sijaintia
+    testMatch: ['**/*.test.js', '**/*.spec.js'],  // Match any .test.js or .spec.js files
 
   };
   
